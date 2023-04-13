671d1041090fa442c05646eb13712ad8d4348521
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-04-13 11:10:12 +0500

    Satr ichidagi so'zlar soni

--- takrorlash/takrorlash/Program.cs
+++ takrorlash/takrorlash/Program.cs
@@ -10,6 +10,24 @@ namespace takrorlash
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter str : ");
+            string str = Console.ReadLine();
+
+            Result(str);
+
+            Console.ReadLine();
+        }
+        public static void Result (string str)
+        {
+            string[] strarr = str.Split(' ');
+            int count = 0;
+
+            for (int i = 0; i < strarr.Length; i++)
+            {
+                count++;
+            }
+
+            Console.WriteLine("Result : " + count);
         }
     }
 }
